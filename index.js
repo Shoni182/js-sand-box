1296be92e7d6013c56127ab5bec1a9c0d4c35802
Shoni182 <100448741+Shoni182@users.noreply.github.com>
2025-10-16 11:34:54 +0000

    Deploying to gh-pages from @ Shoni182/js-sand-box@19dd81144498ea41c98f0235e031f67a3f4830f5 🚀

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();//! 1 ------ Колбек-функції -------
-//! 2 ------ Стрілочні функції -------
-//! 3 ------ Методи map і flatMap -------
-const l=[{ship:"Green",health:100},{ship:"Yellow",health:43},{ship:"Dragon",health:60},{ship:"Star Crush",health:100},{ship:"Tiger",health:33},{ship:"Beta",health:70}],c=l.map(s=>s.health);console.log(c);//! 4 ------ Методи filter і find -------
-//! 5 ------ Методи every, some і reduce -------
-//! 6 ------  Метод toSorted-------
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();//! 2 ------ Прототипи -------
+//! 3 ------  Модуль 6. ООП. Класи -------
+const n={baseSalaty:3e4,overtime:10,rate:20,getWage(){return this.baseSalaty+this.overtime*this.rate}};n.getWage();//! 4 ------  Класи -------
 //# sourceMappingURL=index.js.map
